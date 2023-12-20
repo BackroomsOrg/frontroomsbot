867db99255d1692f652c6523e2410402a5e32787
safar.radim <xsafar27@stud.fit.vutbr.cz>
2023-12-20 22:11:21 +0100

    feat: embed bookmarks

---
+++ frontroomsbot/bookmarks.py
@@ -0,0 +1,28 @@
+from discord import Embed, Colour, User, Message, Attachment
+from datetime import datetime
+from typing import List
+
+def get_bookmark(author: User, message: Message) -> Embed:
+    bookmark = Embed(title="Záložka!", description=message.jump_url, colour=Colour.purple())
+
+    bookmark.set_author(name=author.display_name, icon_url=author.avatar)
+
+    time = datetime.now()
+    bookmark.set_footer(text=f'Záložka vytvořena: {time}')
+
+    bookmark.add_field(name="Obsah:", value=message.content, inline=False)
+    if (message.attachments):
+        add_media(bookmark, message.attachments)
+
+    return bookmark
+
+def add_media(bookmark: Embed, attachments: List[Attachment]):
+    if (len(attachments) > 1):
+        bookmark.add_field(name="", value="[Info] Zpráva ma více příloh!", inline=False)
+
+   
+    if("image" in attachments[0].content_type):
+        bookmark.set_image(url=attachments[0].url)
+    else:
+        bookmark.add_field(name="", value=attachments[0].url)
+
--- frontroomsbot/bot.py
+++ frontroomsbot/bot.py
@@ -3,8 +3,8 @@ import discord
 import httpx
 
 from dotenv import load_dotenv
-
 from random import randint
+from bookmarks import get_bookmark
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -68,7 +68,12 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
     match reaction:
         case "🔖":
             direct = await user.create_dm()
-            await direct.send(message.content)
+
+            if channel == direct:
+                return
+            
+            bookmark = get_bookmark(user, message)
+            await direct.send(embed=bookmark)
         case _:
             return
 
