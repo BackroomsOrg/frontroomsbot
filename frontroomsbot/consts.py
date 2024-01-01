74b754140883ec2b03e808db898cfb24b5d2af79
Adam Hrbac <adam.hrbac@gmail.com>
2024-01-01 23:07:39 +0100

    feat: report errors to a webhook
    Using a webhook prevents using up API limits for
    an error loop.

--- frontroomsbot/consts.py
+++ frontroomsbot/consts.py
@@ -6,5 +6,6 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("GUILD_ID")
 HF_TOKEN = os.getenv("HF_TOKEN")
 DB_CONN = os.getenv("DB_CONN")
+ERROR_WH = os.getenv("ERROR_WH")
 
 COGS_DIR = "cogs"
