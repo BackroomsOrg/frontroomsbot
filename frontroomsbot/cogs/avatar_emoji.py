a13b8a46719a119cadd177d1b6ce662f27cb4e94
Stefan Peknik <speknik@redhat.com>
2024-07-02 10:17:27 +0200

    feat(imitation_fe): clean up

--- frontroomsbot/cogs/avatar_emoji.py
+++ frontroomsbot/cogs/avatar_emoji.py
@@ -4,14 +4,9 @@ from discord.ext import tasks
 import tempfile
 from PIL import Image
 import requests
-import asyncio
-import datetime
 
 from bot import BackroomsBot
 from ._config import ConfigCog, Cfg
-
-
-TIME = datetime.time(hour=2, tzinfo=datetime.timezone.utc)
 
 
 class AvatarEmojiCog(ConfigCog):
@@ -22,22 +17,13 @@ class AvatarEmojiCog(ConfigCog):
         super().__init__(bot)
 
         self.bot = bot
-        self.lock = asyncio.Lock()
-        self.context = ""
 
     @app_commands.command(
-        name="force_avatars_reload",
+        name="reload_avatars",
         description="Force avatars emojis to be reloaded in pantry",
     )
-    async def force_avatars_reload(self, interaction: discord.Interaction):
-        await self.reload_avatars()
-
-    @tasks.loop(time=TIME)
+    @tasks.loop(hours=4 * 24)
     async def reload_avatars(self):
-        async with self.lock:
-            await self.reload_avatars_impl()
-
-    async def reload_avatars_impl(self):
         # create an emoji for each member in the backrooms
         for member in self.bot.backrooms.members:
             await self.create_avatar_emoji_in_pantry(member.name, member.avatar_url)
