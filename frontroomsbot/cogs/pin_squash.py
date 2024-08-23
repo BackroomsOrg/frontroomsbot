904f93aa7978b92695adfc54462932a7c833a2ca
S1ro1 <matej.sirovatka@gmail.com>
2024-08-23 23:48:02 +0200

    Fix: formatting

--- frontroomsbot/cogs/pin_squash.py
+++ frontroomsbot/cogs/pin_squash.py
@@ -3,20 +3,19 @@ from discord import app_commands
 import discord
 from ._config import ConfigCog
 
+
 class PinSquashCog(ConfigCog):
     def __init__(self, bot: BackroomsBot) -> None:
         super().__init__(bot)
-    
+
     @app_commands.command(name="pin_squash", description="Squashes pins")
-    async def pin_squash(
-        self, interaction: discord.Interaction
-    ):
+    async def pin_squash(self, interaction: discord.Interaction):
         channel = interaction.channel
         pins = await channel.pins()
-        
+
         # Create a list to store links to pinned messages
         pin_links = []
-        
+
         # Unpin all messages and collect their links
         for pin in pins:
             await pin.unpin()
@@ -26,10 +25,13 @@ class PinSquashCog(ConfigCog):
             content = "Previously pinned messages:\n" + "\n".join(pin_links)
             new_pin = await channel.send(content)
             await new_pin.pin()
-            
-            await interaction.response.send_message("Pins squashed and new summary pinned.")
+
+            await interaction.response.send_message(
+                "Pins squashed and new summary pinned."
+            )
         else:
             await interaction.response.send_message("No pins found to squash.")
+
 
 async def setup(bot: BackroomsBot) -> None:
     await bot.add_cog(PinSquashCog(bot), guild=bot.backrooms)
